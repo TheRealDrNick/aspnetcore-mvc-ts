2045f5d9767151245c3a47f341aea7a9fff70e35
nicolas.wittmann@ke-elektronik.de <nicolas.wittmann@ke-elektronik.de>
2021-09-06 15:32:15 +0200

    After creating tsconfig file

---
+++ ASPNET-Core-TS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNET_Core_TS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
